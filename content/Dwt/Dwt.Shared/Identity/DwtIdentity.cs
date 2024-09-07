8da072b89058dca1f82eb839fc3cd43cf5eec9ae
Thanh Nguyen <btnguyen2k@gmail.com>
2024-09-08 01:20:19 +1000

    Seed data for Identity

---
+++ content/Dwt/Dwt.Shared/Identity/DwtIdentity.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Claims;
+
+namespace Dwt.Shared.Identity;
+public class DwtIdentity
+{
+	/// <summary>
+	/// Permission to create a new user account.
+	/// </summary>
+	public static readonly Claim CLAIM_PERM_CREATE_USER = new("perm", "create-user");
+
+	/// <summary>
+	/// Permission to create a new application.
+	/// </summary>
+	public static readonly Claim CLAIM_PERM_CREATE_APP = new("perm", "create-app");
+
+	/// <summary>
+	/// Permission to delete an application.
+	/// </summary>
+	public static readonly Claim CLAIM_PERM_DELETE_APP = new("perm", "delete-app");
+
+	/// <summary>
+	/// Permission to modify an application.
+	/// </summary>
+	public static readonly Claim CLAIM_PERM_MODIFY_APP = new("perm", "modify-app");
+}
+
+public class ClaimEqualityComparer : IEqualityComparer<Claim>
+{
+	public static readonly ClaimEqualityComparer Instance = new();
+
+	public bool Equals(Claim? x, Claim? y)
+	{
+		if (x == null && y == null) return true;
+		if (x == null || y == null) return false;
+		return x.Type == y.Type && x.Value == y.Value;
+	}
+
+	public int GetHashCode(Claim obj)
+	{
+		return HashCode.Combine(obj.Type, obj.Value);
+	}
+}

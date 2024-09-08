81975cf865231df42ed9b41ed6e277a62101ca95
Thanh Nguyen <btnguyen2k@gmail.com>
2024-09-08 23:14:29 +1000

    configure custom response for invalid model state

--- content/Dwt/Dwt.Api/Bootstrap/P1000ControllersBootstrapper.cs
+++ content/Dwt/Dwt.Api/Bootstrap/P1000ControllersBootstrapper.cs
@@ -1,11 +1,28 @@
-﻿namespace Dwt.Api.Bootstrap;
+﻿using Dwt.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dwt.Api.Bootstrap;
 
 [Bootstrapper]
 public class ControllersBootstrapper
 {
 	public static void ConfigureBuilder(WebApplicationBuilder appBuilder)
 	{
-		appBuilder.Services.AddControllers();
+		appBuilder.Services.AddControllers()
+			.ConfigureApiBehaviorOptions(options =>
+			{
+				// configure custom response for invalid model state (usually input validation failed)
+				options.InvalidModelStateResponseFactory = context =>
+				{
+					var errors = context.ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage));
+					return new BadRequestObjectResult(new ApiResp<object>
+					{
+						Status = 400,
+						Message = "Invalid model state.",
+						Data = errors
+					});
+				};
+			});
 	}
 
 	public static void DecorateApp(WebApplication app)
